82c9d89707d7140d54092d2b8a06c4cd4b08e2a6
HayneMakken <kirigayoshi26@gmail.com>
2024-11-06 12:40:50 +0200

    Deleted cache

--- 3DActionRoguelike/Library/PackageCache/com.unity.cloud.gltfast@6.8.0/Runtime/Scripts/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2024 Unity Technologies and the glTFast authors
-// SPDX-License-Identifier: Apache-2.0
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("glTFast.Editor")]
-[assembly: InternalsVisibleTo("glTFast.Editor.Tests")]
-[assembly: InternalsVisibleTo("glTFast.Export")]
-[assembly: InternalsVisibleTo("glTFast.Tests")]
